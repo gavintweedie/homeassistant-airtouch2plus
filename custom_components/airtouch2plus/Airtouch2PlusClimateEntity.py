f3fcbea78fb78ba6e5a3ec3127c0900645505311
Gavin Tweedie <59038163+gavintweedie@users.noreply.github.com>
2025-10-03 20:27:57 +0800

    Change target-temp steps to whole numbers, and range 10-35 in Airtouch2PlusClimateEntity.py
    Update Set Temp Defaults in Airtouch2PlusClimateEntity.py
    
    + Change to whole degrees only. 22 is valid. 23 is valid. 22.1 or 22.5 is not.
    + Define supported Min/Max. Was inheriting default of 7-35. Airtouch2+ API doc confirms range is 10-35, confirmed on touchpad itself too.

--- custom_components/airtouch2plus/Airtouch2PlusClimateEntity.py
+++ custom_components/airtouch2plus/Airtouch2PlusClimateEntity.py
@@ -17,7 +17,7 @@ from homeassistant.components.climate import (
     HVACMode,
     UnitOfTemperature,
     ATTR_TEMPERATURE,
-    PRECISION_TENTHS
+    PRECISION_WHOLE
 )
 from homeassistant.helpers.entity import DeviceInfo
 
@@ -34,8 +34,10 @@ class Airtouch2PlusClimateEntity(ClimateEntity):
     #
     # ClimateEntity attributes:
     #
-    _attr_precision: float = PRECISION_TENTHS
-    _attr_target_temperature_step: float = 0.1
+    _attr_max_temp = 35
+    _attr_min_temp = 10
+    _attr_precision = PRECISION_WHOLE
+    _attr_target_temperature_step = PRECISION_WHOLE
     _attr_temperature_unit: str = UnitOfTemperature.CELSIUS
 
     def __init__(self, at2plus_aircon: At2PlusAircon) -> None:
